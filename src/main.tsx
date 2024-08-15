b4363afa099351e481c2ad5220b09f4002c8e022
Evan Ristow <evan.ristow1@gmail.com>
2024-08-15 12:23:50 -0400

    Move home page into pages/Home/index.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
